7ff0a7210166390c84cb7b0c3ef208c8cc5ee210
mgtaylor2 <mgtaylor2@oakland.edu>
2024-03-06 21:15:39 -0500

    SCRUM-55 Adjusted .gitignore and added instructions for readme

--- resume-copilot/src/index.js
+++ resume-copilot/src/index.js
@@ -5,10 +5,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// TODO: REMOVE HARDCODED VALUES, USE .env FILE!
-// Replace these values with your Auth0 domain and client ID
-const auth0Domain = 'dev-6gb5dj5y00rbgay1.us.auth0.com';
-const auth0ClientId = 'iH9BUfGKNVerNf9UvmBDKoRmb51D6bXo';
+// pulls from .env file within src folder
+const auth0Domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const auth0ClientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
