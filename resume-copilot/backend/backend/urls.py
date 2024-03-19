ffbd87df81c57ead9636205c8c971839056ea115
James Horner <james@Jamess-MacBook-Pro.local>
2024-03-19 00:50:33 -0400

    import statements for urls.py

--- resume-copilot/backend/backend/urls.py
+++ resume-copilot/backend/backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .read_resume import readResume
+from .scrape import scrapeWebsite
 
 urlpatterns = [
     path('admin/', admin.site.urls),
