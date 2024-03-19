904279deacfa43bbd3f907ef09520ec1c1014669
James Horner <james@Jamess-MacBook-Pro.local>
2024-03-18 23:50:47 -0400

    changed path to match frontend

--- resume-copilot/backend/backend/urls.py
+++ resume-copilot/backend/backend/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # OpenAI API path
-    path('scrape/', scrapeWebsite, name='scrape'),
+    path('parse_pdf/', scrapeWebsite, name='scrape'),
 ]
