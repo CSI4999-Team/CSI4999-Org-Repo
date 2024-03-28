b8af3f1b54e90447dc5ddf32e8b00a0f40254ef8
stephen <smplaceway@aol.com>
2024-03-28 11:43:23 -0400

    added more informations to TNT page

--- resume-copilot/src/components/TipsAndTricks.js
+++ resume-copilot/src/components/TipsAndTricks.js
@@ -5,6 +5,23 @@ function tipsandtricks(){
 return(
     <div style={{padding: '20px'}}>
     <h1>Tips and tricks to get you started</h1>
+    <p>Lets start with the Skeleton of your resume, A resume consists of several key parts but not necessarily in this order:</p>
+    <ul>
+        <li>Contact information</li>
+        <p>Its best to supply employers with an email, phone number, and a link to your social media (IE: LinkedIn). However don't include things like your birthday, marital status, or photographs</p>
+        <li>Objective header statement</li>
+        <p>This is where you'll provide your goal in the specific field, showcase your key skills, and other unique things that set you apart </p>
+        <li>Summary</li>
+        <li>Key skills</li>
+        <p>What are some main take aways from previous job experiences? tools you learned from your educational career?</p>
+        <li>Education and training</li>
+        <p>Any education pertaining to your career field should be listed be sure to include the school name, location, your time span spent there, and your degree.
+        Any training you've completed for certifications or other projects can be included.   </p>
+        <li>Experience</li>
+        <li>Activities and affiliations</li>
+        <li>Honors and awards</li>
+    </ul>
+    <p>Now that you know what you need lets </p>
     </div>
 );
 }
