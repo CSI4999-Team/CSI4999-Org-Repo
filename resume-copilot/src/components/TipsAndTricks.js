b7dec5c3bf6e47d3796e5a2ed9a58431254d58b9
stephen <smplaceway@aol.com>
2024-03-28 09:59:49 -0400

    added tips and tricks page

--- resume-copilot/src/components/TipsAndTricks.js
+++ resume-copilot/src/components/TipsAndTricks.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './PrivacyPolicy.css';
 
 function tipsandtricks(){
-
+return(
+    <div style={{padding: '20px'}}>
+    <h1>Tips and tricks to get you started</h1>
+    </div>
+);
 }
 export default tipsandtricks;
